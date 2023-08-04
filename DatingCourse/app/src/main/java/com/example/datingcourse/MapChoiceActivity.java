12f30bd582b9075b0917717e93732fb12a2afad3
gb <dbsruaqls123@gmail.com>
2023-08-04 15:54:01 +0900

    Test

---
+++ DatingCourse/app/src/main/java/com/example/datingcourse/MapChoiceActivity.java
@@ -0,0 +1,15 @@
+// 지도 선택 클래스
+package com.example.datingcourse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_choice);
+    }
+}
