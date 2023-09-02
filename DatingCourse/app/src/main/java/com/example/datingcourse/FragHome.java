e3eedb31959564c25886cfb2d9f05d26bfce0fe3
gb <dbsruaqls123@gmail.com>
2023-09-02 23:45:06 +0900

    9/02 공모전 준비 끝

--- DatingCourse/app/src/main/java/com/example/datingcourse/FragHome.java
+++ DatingCourse/app/src/main/java/com/example/datingcourse/FragHome.java
@@ -1,4 +1,7 @@
 package com.example.datingcourse;
+import static android.content.ContentValues.TAG;
+
+import android.app.Activity;
 import android.os.Bundle;
 
 import android.app.AlertDialog;
@@ -7,6 +10,7 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +18,8 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.VideoView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.annotation.NonNull;
@@ -24,10 +30,27 @@ import androidx.viewpager.widget.ViewPager;
 import java.util.Timer;
 import java.util.TimerTask;
 
+
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.OnPaidEventListener;
+import com.google.android.gms.ads.ResponseInfo;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+
+
 public class FragHome extends Fragment {
     private View view;
     private ViewPager viewPager;
     private int currentPage = 0;
+    private InterstitialAd mInterstitialAd;
     private int[] imageIds = {R.drawable.ad1, R.drawable.ad2, R.drawable.ad3,R.drawable.ad4};
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,6 +68,12 @@ public class FragHome extends Fragment {
         viewPager = view.findViewById(R.id.viewPager);
         ImagePagerAdapter adapter = new ImagePagerAdapter(getActivity(), imageIds);
         viewPager.setAdapter(adapter);
+
+        MobileAds.initialize(getActivity(), new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
 
         final Handler handler = new Handler();
         final Runnable runnable = new Runnable() {
@@ -256,6 +285,7 @@ public class FragHome extends Fragment {
         mbti_e.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                loadFrontAd();
                 String mbti = "e";
                 Intent intent = new Intent(getActivity(),MBTI.class);
                 intent.putExtra("mbti", mbti);
@@ -290,6 +320,70 @@ public class FragHome extends Fragment {
 
 
     }
+    private void loadFrontAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        // ca-app-pub 부분은 나중에 마켓등록시 변경하면 된다.
+        InterstitialAd.load(getActivity(), "ca-app-pub-3940256099942544/1033173712", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // 광고 로드가 성공하면 보여 주는 로직을 타면 된다.
+                        mInterstitialAd = interstitialAd;
+                        showFrontAd();
+                        Log.i(TAG, "onAdLoaded");
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.d(TAG, loadAdError.toString());
+                        mInterstitialAd = null;
+                    }
+                });
+    }
+    private void showFrontAd() {
+
+        // callback을 통해 광고의 상태를 확인 해서 추가적인 동작을 할 수 있다.
+        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+            @Override
+            public void onAdClicked() {
+                // Called when a click is recorded for an ad.
+                Log.d(TAG, "Ad was clicked.");
+            }
+
+            @Override
+            public void onAdDismissedFullScreenContent() {
+                // Called when ad is dismissed.
+                // Set the ad reference to null so you don't show the ad a second time.
+                Log.d(TAG, "Ad dismissed fullscreen content.");
+                mInterstitialAd = null;
+            }
+
+            @Override
+            public void onAdFailedToShowFullScreenContent(AdError adError) {
+                // Called when ad fails to show.
+                Log.e(TAG, "Ad failed to show fullscreen content.");
+                mInterstitialAd = null;
+            }
+
+            @Override
+            public void onAdImpression() {
+                // Called when an impression is recorded for an ad.
+                Log.d(TAG, "Ad recorded an impression.");
+            }
+
+            @Override
+            public void onAdShowedFullScreenContent() {
+                // Called when ad is shown.
+                Log.d(TAG, "Ad showed fullscreen content.");
+            }
+        });
+
+        // show 호출
+        mInterstitialAd.show(getActivity());
+    }
+
 
 }
 
